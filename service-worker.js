fa063e9f7728ce59d3b43ce7606ee55966b03367
beacasas943-beep <beacasas943@gmail.com>
2026-09-13 13:56:55 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='room-client-rc44-v1';
+const CACHE='room-client-rc441-v1';
 const CORE=['/cliente.html','/app.css','/js/portal.js','/js/room-data.js','/config.js','/assets/room-mark.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
